6b67fc5771b992c42109e13f4df3cb7b0cbfefef
yiliangwang <yiliangwang@tencent.com>
2025-07-15 17:23:44 +0800

    Chat_SDK_SwiftPM upgrade version 8.6.7019

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "ImSDK_Plus_xcframework",
-                      url: "https://im.sdk.cloud.tencent.cn/download/plus/8.5.6864/ImSDK_Plus_8.5.6864.xcframework.zip",
-                      checksum: "0f8bc28c73dfd1a6efb5cb84bc15c6b86fa490336b42cb375e3349622a9cd36c")
+                      url: "https://im.sdk.cloud.tencent.cn/download/plus/8.6.7019/ImSDK_Plus_8.6.7019.xcframework.zip",
+                      checksum: "4aad47a5f8e174546fbb7930c43a5125f7d1b9e9694cf91b583a3e09104334ed")
     ]
 )
